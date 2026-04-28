0f4ea0e7953ffbbac9c476959057f0b96dee68b6
Alex Barber <a.j.barber@lancaster.ac.uk>
2026-04-28 14:17:45 +0100

    Improved piece swap logic. Now also moves until hitting another object or the edge.

--- GamePiece.java
+++ GamePiece.java
@@ -27,7 +27,70 @@ public class GamePiece extends JButton {
         this.y = y;
     }
 
-    public void movementCheck() {
+    public void movement(GamePiece[] array, GamePiece first, GamePiece second) {
+        // Check GamePieces are valid (only swaps with blank tiles)
+        if (first == null || second == null) {
+            return; 
+        }
+        if (second instanceof Tree || second instanceof Snowball || second instanceof SnowmanHead) {
+            return;
+        }
 
+        // Find all x,y coords
+        int firstX = first.returnX();
+        int firstY = first.returnY();
+        int secondX = second.returnX();
+        int secondY = second.returnY();
+
+        // Find direction of movement
+        int dx = secondX - firstX;
+        int dy = secondY - firstY;
+
+        // Check tiles are next to one another
+        if (!((dx == 0 && Math.abs(dy) == 1) || (dy == 0 && Math.abs(dx) == 1))) {
+            return;
+        }
+
+        // Swap coords
+        first.setX(secondX);
+        first.setY(secondY);
+        second.setX(firstX);
+        second.setY(firstY);
+
+        // Find location in array
+        int firstIndex = -1;
+        int secondIndex = -1;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == first) {
+                firstIndex = i;
+            }
+            if (array[i] == second) {
+                secondIndex = i;
+            }
+        }
+
+        // Check not out of array
+        if (firstIndex == -1 || secondIndex == -1) {
+            return;
+        }
+
+        // Swap locations in array
+        GamePiece temp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = temp;
+        
+        // Find next tile to be swapped
+        GamePiece nextSwap = null;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].returnX() == first.returnX() + dx && array[i].returnY() == first.returnY() + dy) {
+                nextSwap = array[i];
+                break;
+            }
+        }
+
+        if (nextSwap != null) {
+            movement(array, first, nextSwap);
+        }
     }
+
 }
